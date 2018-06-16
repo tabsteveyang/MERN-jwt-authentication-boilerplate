36573d12f4d32ef3d33bc581333e6d3ccc3c12d8
Tab <tabsteveyang@gmail.com>
2018-06-16 09:41:21 +0000

    remove the parts that are not essential and update the package.json file to avoid the potential security vulnerability shown by Github.

--- server/mongodb/db.js
+++ server/mongodb/db.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const { User } = require('./schema/User');
-const { Group } = require('./schema/Group');
 
 const dbhost = process.env.DBHOST;
 mongoose.connect(dbhost);
 
-module.exports = {
-    User,
-    Group,
-};
+module.exports = { User };
